076af34af327e2134b537bdb21b5ac58b41ce478
Hari545543 <98028428+Hari545543@users.noreply.github.com>
2022-04-09 18:38:00 +0530

    Update Phonebook_with_save_and_load.cpp

--- PhoneBook using BST/Phonebook_with_save_and_load.cpp
+++ PhoneBook using BST/Phonebook_with_save_and_load.cpp
@@ -26,10 +26,14 @@ void save();
 void savetofile(char i,int hash);
 void savein(struct node *head,char i);
 struct node *input(struct node *);
+struct node* deleteNode(struct node*root[], char key[]);
+struct node* deleteNodeinside(struct node*, char key[]);
+struct node *findmin(struct node *tree);
 void inorder(int i);
 void inorderinside(struct node *);
-struct node *insert(struct node *);
+struct node *insert(struct node *head[],struct node *new_node);
 struct node *insertinside(struct node *,struct node *);
+struct node *findmin(struct node *tree);
 struct node *head[26];
 
 
@@ -49,14 +53,14 @@ new_node -> left = NULL;
 new_node -> right = NULL;
 head[hash] = new_node;
 }
-else{
-if(strcmp(head[hash] -> name,new_node -> name) > 0){
-head[hash] -> left = insertinside(head[hash] -> left, new_node);
-}
-else{
-head[hash] -> right = insertinside(head[hash] -> right, new_node);
-}
-}
+	else{
+		if(strcmp(head[hash] -> name,new_node -> name) > 0){
+			head[hash] -> left = insertinside(head[hash] -> left, new_node);
+		}
+		else{
+			head[hash] -> right = insertinside(head[hash] -> right, new_node);
+		}
+	}
 //return tree;
 //return head[hash];
 }
@@ -66,6 +70,7 @@ head[hash] -> right = insertinside(head[hash] -> right, new_node);
 int main(){
 struct node *n_node;
 char ans;
+int hash;
 for(int i=0;i<=26;i++)
     {
     	head[i] = (struct node*)malloc(sizeof(struct node));
@@ -73,11 +78,12 @@ for(int i=0;i<=26;i++)
          
      }
 int option;
+char key[40];
 printf("\n**********************************************************************************************PHONEBOOK************************************************************************************************************");
 printf("\n-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 load();
 do{
-	cout<<"\nMENU\n 1)INSERT\n2)PRINT\n3)SEARCH(*for representation)\n4)EXIT\n5)ENTER YOU CHOICE\n";
+	cout<<"\nMENU\n 1)INSERT\n2)PRINT\n3)SEARCH(*for representation)\n4)DELETE A CONTACT\n5)EXIT\nENTER YOU CHOICE\n";
 	cin>>option;
 	
 	switch(option)
@@ -102,6 +108,11 @@ do{
 		break;
 		
 		case 4:
+		cout<<"ENTER THE NAME\n";
+		cin>>key;
+		key[0] = toupper(key[0]);
+		hash = int (key[0]) % 65;
+		deleteNodeinside(head[hash],key);
 		break;
 		
 		default:
@@ -109,7 +120,7 @@ do{
 	
 	}
 	
-}while(option!=4);
+}while(option!=5);
 save();
 return 0;
 }
@@ -127,6 +138,8 @@ struct node *insertinside(struct node *head,struct node *new_node )
 	else
 	{
 		
+
+	
 		if(strcmp(head -> name, new_node -> name) > 0)
 		{
 			head -> left = insertinside(head -> left, new_node);
@@ -136,7 +149,7 @@ struct node *insertinside(struct node *head,struct node *new_node )
 			head -> right = insertinside(head -> right, new_node);
 		}
 	}
-	return head;
+	
 }
 
 
@@ -293,10 +306,13 @@ void savetofile(char i,int hash){
 	fn[45] = i;
 	cout<<"\n\n\n\t\tPlease wait for a moment\n";
 	cout<<"\t\tSaving data to file\n\n";
-	savein(head[hash] -> left,i);	
-	ob.open(fn,ios::out | ios::app);
-	ob<<"\n"<<head[hash] -> name<<"\n"<<head[hash] -> number<<"\n"<<head[hash] -> address<<"\n"<<head[hash] -> email;
+	ob.open(fn,ios::trunc);
 	ob.close();
+	
+	ob.open(fn,ios::out |ios::app);
+	ob<<"\n"<<head[hash] -> name<<"\n"<<head[hash] -> number<<"\n"<<head[hash] -> address<<"\n"<<head[hash] -> email;
+	ob.close();	
+	savein(head[hash] -> left,i);
 	savein(head[hash] -> right,i);
  
 }
@@ -307,12 +323,107 @@ void savein(struct node *head,char i){
 	fn[45] = i;
 	if(head != NULL){
 		
-		savein(head -> left,i);
+		
 		ob.open(fn,ios::out | ios::app);
 		ob<<"\n"<<head -> name<<"\n"<<head -> number<<"\n"<<head -> address<<"\n"<<head -> email;
 		ob.close();
+		savein(head -> left,i);
 		savein(head -> right,i);
-		
 	}
 }
-  
+ /* 
+struct node* deleteNode(struct node* root[], char key[])
+{
+    int hash = int (key[0]) % 65;
+	if (root[hash] == NULL)
+        return root[hash];
+ 
+
+    if (strcmp(root[hash] -> name,key) > 0)
+        root[hash]->left = deleteNodeinside(root[hash]->left, key);
+ 
+
+    else if (strcmp(root[hash] -> name,key) < 0)
+        root[hash]->right = deleteNodeinside(root[hash]->right, key);
+ 
+
+    else {
+    	
+        if (root[hash]->left == NULL) {
+            struct node* temp = root[hash]->right;
+            free(root[hash]);
+            return temp;
+        }
+        else if (root[hash]->right == NULL) {
+            struct node* temp = root[hash]->left;
+            free(root[hash]);
+            return temp;
+        }
+ 
+
+        struct node* temp = findmin(root[hash]->right);
+ 
+
+        root[hash]= temp;
+ 
+
+        root[hash]->right = deleteNodeinside(root[hash]->right, temp->name);
+    }
+    return root[hash];
+}*/
+
+struct node* deleteNodeinside(struct node* root, char key[])
+{
+	struct node* temp;
+	if (root== NULL)
+        return root;
+ 
+
+    if (strcmp(root -> name,key) > 0){
+    	 root->left = deleteNodeinside(root->left, key);
+	}
+       
+ 
+
+    else if (strcmp(root -> name,key) < 0){
+    	 root->right = deleteNodeinside(root->right, key);
+	}
+       
+ 
+
+    else {
+    	
+        if (root->left == NULL) {
+            temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL) {
+            temp = root->left;
+            free(root);
+            return temp;
+        }
+ 
+
+        temp = findmin(root->right);
+ 
+
+        strcpy(root -> name,temp -> name);
+		strcpy(root -> number,temp ->  number);
+		strcpy(root -> address,temp ->  address);
+		strcpy(root -> email,temp ->  email);
+
+        root->right = deleteNodeinside(root->right, temp->name);
+    }
+    return root;
+}
+
+struct node* findmin(struct node* tree)
+{
+    struct node* node1 = tree;
+ 
+    while (node1 != NULL && node1->left != NULL)
+        node1 = node1->left;
+ 
+    return node1;
+}
